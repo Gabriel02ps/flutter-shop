bb740c885207fc5a0b7b4afcc12920e683640b9c
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-19 10:01:50 -0300

    feat: add change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/providers/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
-import 'components/product_detail_page.dart';
+import 'pages/product_detail_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +16,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.purple,
-        hintColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.purple,
+          hintColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: const ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (context) => const ProductDetailPage()
+        },
       ),
-      home: ProductsOverviewPage(),
-      routes: {AppRoutes.productDetail: (context) => const ProductDetailPage()},
     );
   }
 }
