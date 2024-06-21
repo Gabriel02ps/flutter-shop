8e0f7c7c5d50012780a2609e0df89a2afb5c1422
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-21 11:28:32 -0300

    feat: add optimistic exclusion

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/models/product.dart';
+import 'package:shop/exceptions/http_exception.dart';
 
 class ProductList with ChangeNotifier {
   final _baseUrl =
@@ -106,12 +107,26 @@ class ProductList with ChangeNotifier {
     return Future.value();
   }
 
-  void removeProduct(Product product) {
+  Future<void> removeProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
-      _items.removeWhere((p) => p.id == product.id);
+      final product = _items[index];
+      _items.remove(product);
       notifyListeners();
+
+      final response = await http.delete(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+      );
+
+      if (response.statusCode >= 400) {
+        _items.insert(index, product);
+        notifyListeners();
+        throw HttpException(
+          msg: 'Não foi possível excluir o produto.',
+          statusCode: response.statusCode,
+        );
+      }
     }
   }
 }
