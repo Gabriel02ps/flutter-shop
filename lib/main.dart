4fdbdd10335ed8225f5bc13df564b9523559bd59
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-21 17:18:08 -0300

    feat: add login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
+import 'package:shop/pages/auth_page.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/product_page.dart';
-import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:provider/provider.dart';
@@ -35,15 +35,22 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
+        home: const AuthPage(),
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primaryColor: Colors.purple,
           hintColor: Colors.deepOrange,
           fontFamily: 'Lato',
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(Colors.purple),
+              foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+            ),
+          ),
         ),
-        home: const ProductsOverviewPage(),
         routes: {
+          AppRoutes.auth: (ctx) => const AuthPage(),
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
           AppRoutes.cart: (ctx) => const CartPage(),
           AppRoutes.orders: (ctx) => const OrdersPage(),
