4eb81c0076844fde96a131192da66285c192d371
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-21 14:43:54 -0300

    feat: add loading orders

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -35,14 +35,7 @@ class CartPage extends StatelessWidget {
                       label: Text('R\$${cart.totalAmount.toStringAsFixed(2)}',
                           style: const TextStyle(color: Colors.white))),
                   const Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      Provider.of<OrderList>(context, listen: false)
-                          .addOrder(cart);
-                      cart.clear();
-                    },
-                    child: const Text('COMPRAR'),
-                  )
+                  CartButton(cart: cart)
                 ],
               ),
             ),
@@ -56,5 +49,39 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  const CartButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const CircularProgressIndicator()
+        : TextButton(
+            onPressed: widget.cart.itemsCount == 0
+                ? null
+                : () async {
+                    setState(() => _isLoading = true);
+                    await Provider.of<OrderList>(context, listen: false)
+                        .addOrder(widget.cart);
+                    widget.cart.clear();
+                    setState(() => _isLoading = false);
+                  },
+            child: const Text('COMPRAR'),
+          );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const String productBaseUrl =
       'https://flutter-shop-22868-default-rtdb.firebaseio.com/products';
+  static const String orderBaseUrl =
+      'https://flutter-shop-22868-default-rtdb.firebaseio.com/orders';
 }
